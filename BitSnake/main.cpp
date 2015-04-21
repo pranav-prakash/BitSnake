748d01edf7174f6f21ecd0ad9d6d9491dc94bd6e
Pranav Prakash <pranav-prakash@users.noreply.github.com>
2015-04-20 17:18:53 -0700

    Initial Commit

---
+++ BitSnake/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  BitSnake
+//
+//  Created by Pranav Prakash on 4/20/15.
+//  Copyright (c) 2015 Pranav Prakash. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
